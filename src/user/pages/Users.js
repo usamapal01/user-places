4836166edb92b6cfb39b3ef1d4a7475b98d0cbb9
usamapal01 <usamapal01@users.noreply.github.com>
2024-08-16 10:03:02 -0700

    Adding Buttons
    Also added CSS for buttons

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -9,7 +9,7 @@ const Users = () => {
       name: "Usama Pal",
       image:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtuphMb4mq-EcVWhMVT8FCkv5dqZGgvn_QiA&s",
-      places: 3,
+      places: 1,
     },
   ];
 
